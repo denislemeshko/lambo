30b81cdbd87a046eaafd6d151f8d0f0ed6959f5b
denislemeshko <lemeshko.denis@yandex.ru>
2023-01-18 13:19:16 +0300

    final-mobile

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBtn.addEventListener('click', () => {
+    menuMobile.classList.toggle('menu--open');
+});
+
+
 // swiper
 
 
@@ -16,6 +24,15 @@ const swiper = new Swiper('.swiper', {
       nextEl: '.swiper-button-right',
       prevEl: '.swiper-button-left',
     },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        }
+    }
 
   });
 
